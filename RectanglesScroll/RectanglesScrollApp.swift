01ad9c38a8ace4cda14109af532c22e568f2609a
Osver Fernandez <osverdev@icloud.com>
2022-07-12 11:49:42 -0600

    Initial Commit

---
+++ RectanglesScroll/RectanglesScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectanglesScrollApp.swift
+//  RectanglesScroll
+//
+//  Created by Osver Fernandez on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RectanglesScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
